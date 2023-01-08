82c4104fa4e1e8c91e55eba323240883cbf17abf
cryptoIRAfinance <support@cryptoira.finance>
2023-01-08 17:21:20 -0600

    svg color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         <img
           src={toggle ? close : menu}
           alt="menu"
-          className="w-[28px] h-[28px] object-contain"
+          className="w-[32px] h-[32px] object-contain"
           onClick={() => setToggle(!toggle)}
         />
 
