64a23d70debbad7c6916e46fc05323db4858df1d
cryptoIRAfinance <support@cryptoira.finance>
2023-01-12 17:14:56 -0600

    d

--- src/components/Applications.jsx
+++ src/components/Applications.jsx
@@ -18,7 +18,7 @@ const CardDeal = () => (
     </div>
 
     <div className={layout.sectionImg}>
-      <img src={card} alt="billing" className="w-[85%] h-[85%]" />
+      <img src={card} alt="billing" className="w-[85%] h-[100%]" />
     </div>
   </section>
 );
