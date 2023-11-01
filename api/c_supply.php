cd1c8cbb515f9066d5155300c7fa83205294b9f8
cryptoIRAfinance <support@cryptoira.finance>
2023-11-01 13:25:26 -0500

    api troubleshooting

--- api/c_supply.php
+++ api/c_supply.php
@@ -13,7 +13,7 @@ $ur1 = http_request("https://api.bscscan.com/api?module=account&action=tokenbala
 $ur2 = http_request("https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xDd25E1955FD9F7B3aBE83CC419070A7ace104DCE&address=0x7ee058420e5937496f5a2096f04caa7721cf70cc&tag=latest&apikey=JAGR4Z37B7PRU9GBQTHR5G6E2QRXG8BA4D");
 $ur3 = http_request("https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xDd25E1955FD9F7B3aBE83CC419070A7ace104DCE&address=0x000000000000000000000000000000000000dead&tag=latest&apikey=JAGR4Z37B7PRU9GBQTHR5G6E2QRXG8BA4D");
 $ur4 = http_request("https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xDd25E1955FD9F7B3aBE83CC419070A7ace104DCE&address=0xAe50A9404e79160c51e7266021B644B906972B3F&tag=latest&apikey=JAGR4Z37B7PRU9GBQTHR5G6E2QRXG8BA4D");
-$ur5 = http_request("https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xDd25E1955FD9F7B3aBE83CC419070A7ace104DCE&address=0xfB5f80106F71E7f9850aF204da259a467A7Cc0DD&tag=latest&apikey=JAGR4Z37B7PRU9GBQTHR5G6E2QRXG8BA4D");
+// $ur5 = http_request("https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=0xDd25E1955FD9F7B3aBE83CC419070A7ace104DCE&address=0xfB5f80106F71E7f9850aF204da259a467A7Cc0DD&tag=latest&apikey=JAGR4Z37B7PRU9GBQTHR5G6E2QRXG8BA4D");
 
 $j1 = json_decode($ur1, true);
 $j2 = json_decode($ur2, true);
@@ -25,15 +25,15 @@ $re1 = $j1["result"];
 $re2 = $j2["result"];
 $re3 = $j3["result"];
 $re4 = $j4["result"];
-$re5 = $j5["result"];
+// $re5 = $j5["result"];
 
 $s1 =$re1/ 1e5;
 $s2 =$re2/ 1e5;
 $s3 =$re3/ 1e5;
 $s4 =$re4/ 1e5;
-$s5 =$re5/ 1e5;
+// $s5 =$re5/ 1e5;
 
-$cal = 10000000 - ($s1 + $s2 + $s3 + $s4 + $s5);
+$cal = 10000000 - ($s1 + $s2 + $s3 + $s4);
 print number_format($cal, 5, '.', '');
 //print $cal;
 ?>
