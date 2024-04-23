015664d2716d87f75bf8c84e7312c3cb34a48ab1
cryptoIRAfinance <support@cryptoira.finance>
2024-04-23 11:17:16 -0500

    Update get_users.js

--- api/get_users.js
+++ api/get_users.js
@@ -1,9 +1,10 @@
 import { readFile } from 'fs/promises';
+import path from 'path';
 
 export default async function handler(req, res) {
     try {
         // Read the JSON file
-        const jsonData = await readFile('./api/py_bot_users.json', 'utf8');
+        const jsonData = await readFile(path.join(__dirname, 'py_bot_users.json'), 'utf8');
         const data = JSON.parse(jsonData);
 
         // Return the JSON data
